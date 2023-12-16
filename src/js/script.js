2f7e7427fa3fcd9845a423ba422984e463dca4a4
thefireodessa <thefireodessa@gmail.com>
2023-12-16 13:34:23 +0200

    hw-2 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+let planetName = "Earth";
+let userName = prompt('Enter your name');
+alert('Hello, '+userName+'! How are you?');
+
+let result = "10" + 10;
+console.log(result);
