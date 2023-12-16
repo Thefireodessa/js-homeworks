1166b28f86ec6f7f5928f4d03dc94aadedbddc1b
thefireodessa <thefireodessa@gmail.com>
2023-12-16 17:11:41 +0200

    hw-3 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let number1 = prompt("Type your first number");
+let number2 = prompt("Type your second number");
+
+console.log(`Sum: ${number1} + ${number2} = ${number1 + number2}`);
+console.log(`Difference: ${number1} - ${number2} = ${number1 - number2}`);
+console.log(`Multiplication: ${number1} * ${number2} = ${number1 * number2}`);
+console.log(`Division: ${number1} / ${number2} = ${number1 / number2}`);
