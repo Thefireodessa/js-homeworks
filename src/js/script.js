ecaaa258ab1c8a8add8d5d5fb385e63b60ff567a
thefireodessa <thefireodessa@gmail.com>
2023-12-18 22:28:51 +0200

    hw-4 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const firstString = prompt("Type your first string");
+const secondString = prompt("Type your second string");
+const thirdString = prompt("Type your third string");
+
+console.log(firstString + thirdString + secondString);
+
+const number = prompt("Type your five-digit number");
+
+const separateNumber = number.toString().split('');
+console.log(separateNumber.join(' '));
