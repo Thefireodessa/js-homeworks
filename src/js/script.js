48c7d9c18d5477a22782db76eaa7f52e3639b4dd
thefireodessa <thefireodessa@gmail.com>
2023-12-18 19:29:56 +0200

    script.js update

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let number1 = prompt("Type your first number");
-let number2 = prompt("Type your second number");
+const number1 = prompt("Type your first number");
+const number2 = prompt("Type your second number");
 
 console.log(`Sum: ${number1} + ${number2} = ${number1 + number2}`);
 console.log(`Difference: ${number1} - ${number2} = ${number1 - number2}`);
