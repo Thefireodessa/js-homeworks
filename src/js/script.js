8544e40f5511d76de203611aa34640135fb4eb7e
thefireodessa <thefireodessa@gmail.com>
2024-02-28 22:11:01 +0200

    hw-8 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let age = prompt("How old are you?");
+
+if (!isNaN(age) && age > 0) {
+    let futureAge = parseInt(age) + 10;
+    alert("10 years later you will be" + futureAge + " years old.");
+} else {
+    alert("You should type a number.");
+}
