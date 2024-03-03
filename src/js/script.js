1a1a967a88524624238d0aa689b8c1ad465f4fda
thefireodessa <thefireodessa@gmail.com>
2024-03-03 21:19:55 +0200

    third task hw-12 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
+
+let sum = Object.values(salaries).reduce((acc, salary) => acc + salary, 0);
+
+console.log('Salary amount:', sum);
