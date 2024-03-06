ab118ca919485ee243fa28242ae13e09f53de6c1
thefireodessa <thefireodessa@gmail.com>
2024-03-06 22:44:48 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const number1 = parseFloat(prompt("Type your first number"));
+const number2 = parseFloat(prompt("Type your second number"));
+const number3 = parseFloat(prompt("Type your third number"));
+
+if (isNaN(number1) || isNaN(number2) || isNaN(number3)) {
+    alert("Please, type correct number");
+} else {
+    var average = (number1 + number2 + number3) / 3;
+
+    alert("Average is " + average);
+}
