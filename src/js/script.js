b0a3054b7a2eff918d4edb9b304fd3fab3881157
thefireodessa <thefireodessa@gmail.com>
2024-03-09 13:10:58 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let numOrStr = prompt('Введіть число або рядок');
+console.log(numOrStr);
+
+switch (true) {
+    case numOrStr === null:
+        console.log('Ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Порожній рядок');
+        break;
+    case isNaN(+numOrStr):
+        console.log('Число не є коректним');
+        break;
+    default:
+        console.log('OK!');
+}
+
+
