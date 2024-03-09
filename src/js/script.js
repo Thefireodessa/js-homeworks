b05d3d9bd9c92d837203f92282fb119f9269de2c
thefireodessa <thefireodessa@gmail.com>
2024-03-09 13:49:03 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const userAgeInput = prompt('Введіть ваш вік');
+
+if (!isNaN(userAgeInput) && userAgeInput !== null) {
+    const userAge = parseInt(userAgeInput);
+
+    let ageWord;
+    if (userAge % 10 === 1 && userAge % 100 !== 11) {
+        ageWord = 'рік';
+    } else if ((userAge % 10 >= 2 && userAge % 10 <= 4) && (userAge % 100 < 10 || userAge % 100 >= 20)) {
+        ageWord = 'роки';
+    } else {
+        ageWord = 'років';
+    }
+
+    alert(`Ваш вік ${userAge} ${ageWord}`);
+} else {
+    alert('Будь ласка, введіть коректне число');
+}
