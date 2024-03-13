435032258032a40d9012422533dee6120f50fd04
thefireodessa <thefireodessa@gmail.com>
2024-03-13 21:56:39 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let sum = 0;
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        continue; // Пропустити парні числа
+    }
+
+    sum += i;
+}
+
+console.log(`Сума непарних чисел від 1 до 20: ${sum}`);
