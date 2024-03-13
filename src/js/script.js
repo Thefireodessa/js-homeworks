84dfe1ede9f687eef1cbf58deaee3ac25b2db6cd
thefireodessa <thefireodessa@gmail.com>
2024-03-13 21:54:43 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const userInput = prompt('Введіть число:');
+const number = parseInt(userInput);
+
+if (isNaN(number) || number < 1) {
+    console.log('NaN');
+} else {
+    let smallestDivisor = 2;
+
+    while (smallestDivisor <= Math.sqrt(number)) {
+        if (number % smallestDivisor === 0) {
+            console.log(`Мінімальний дільник числа ${number}: ${smallestDivisor}`);
+            break;
+        }
+        smallestDivisor++;
+    }
+
+    if (smallestDivisor > Math.sqrt(number)) {
+        console.log(`Число ${number} є простим, мінімальний дільник - саме число.`);
+    }
+}
