33a53cde4c0f1e34416a4a3405957d1d3589bbae
thefireodessa <thefireodessa@gmail.com>
2024-03-13 21:50:56 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+
+// Знайдіть суму елементів масиву
+let sum = 0;
+for (const num of arr) {
+    sum += num;
+}
+console.log(`Сума елементів масиву: ${sum}`);
+
+// Знайдіть суму квадратів елементів масиву
+let squareSum = 0;
+for (const num of arr) {
+    squareSum += Math.pow(num, 2);
+    // або squareSum += num ** 2;
+}
+console.log(`Сума квадратів елементів масиву: ${squareSum}`);
