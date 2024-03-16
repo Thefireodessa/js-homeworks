dd6db2f705bb60f2c7ea84eceb373f83186368f1
thefireodessa <thefireodessa@gmail.com>
2024-03-16 17:59:56 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function filterPositiveNumbers(arr) {
+    if (!Array.isArray(arr)) {
+        return 'Помилка: переданий параметр має бути масивом.';
+    }
+
+    const positiveNumbers = [];
+
+    if (arr.length === 0) {
+        return null;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positiveNumbers.push(arr[i]);
+        }
+    }
+
+    if (positiveNumbers.length === 0) {
+        return null;
+    }
+
+    return positiveNumbers;
+}
+
+const arr = [1, 2, 3, -1, -2, -3];
+console.log(filterPositiveNumbers(arr)); // [1, 2, 3]
