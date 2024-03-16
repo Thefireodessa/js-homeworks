a592cbc40fe6a3fb316ab206044fafe98c9862aa
thefireodessa <thefireodessa@gmail.com>
2024-03-16 12:29:18 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function generateKey(length, characters) {
+    const charactersLength = characters.length;
+    let key = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        key += characters.charAt(randomIndex);
+    }
+
+    return key;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
