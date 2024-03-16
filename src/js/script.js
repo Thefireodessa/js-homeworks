d7dada112e03147aa47c4e0d32b78963373b0127
thefireodessa <thefireodessa@gmail.com>
2024-03-16 12:37:02 +0200

    task completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function power(base, exponent = 1) {
+    if (typeof base !== 'number' || typeof exponent !== 'number' || isNaN(base) || isNaN(exponent)) {
+        return 'Помилка. Будь ласка, введіть числа.';
+    }
+    return Math.pow(base, exponent);
+}
+
+const inputBase = prompt('Введіть число:');
+const inputExponent = prompt('Введіть ступінь:');
+
+const base = parseFloat(inputBase);
+const exponent = parseFloat(inputExponent);
+
+const result = power(base, exponent);
+alert(`Результат: ${base} в ступені ${exponent} дорівнює ${result}`);
